723b2ff4d8a24eba546f0abe8e21ae5dc61dbcb7
Samuel <samuelfreitassoares@outlook.com>
2024-04-29 19:41:03 -0300

    New Vers.

---
+++ feedback360/api/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Coordenadores)
+admin.site.register(Cursos)
+admin.site.register(Aluno)
+admin.site.register(Disciplina)
+admin.site.register(Competencias)
+admin.site.register(Professores)
+admin.site.register(Atividade)
+admin.site.register(Turma)
+admin.site.register(Nota)
+admin.site.register(Turma_Aluno)
+
